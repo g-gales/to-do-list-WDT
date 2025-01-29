bc2c421e1a7fb050dbb77a0d221ba1dab54cc517
g-gales <graysongales15@gmail.com>
2025-01-28 20:26:49 -0500

    admin removal

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import pluginJs from "@eslint/js";
 export default [
   {
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    ignores: ["dist/", "docs/"],
+    ignores: ["dist/*", "docs/*"],
   },
   pluginJs.configs.recommended,
 ];
